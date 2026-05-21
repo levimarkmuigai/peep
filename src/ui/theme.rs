e61501eb1c6e27527cf2ce785605f7c8847f19fb
levimarkmuigai <levimarkmuigai@gmail.com>
2026-05-21 09:39:11 +0300

    feat: theme colors

--- src/ui/theme.rs
+++ src/ui/theme.rs
@@ -1,1 +1,21 @@
+use ratatui::style::{Color, palette::tailwind};
 
+pub struct Theme {
+    pub border: Color,
+    pub app_title: Color,
+    pub stats_warning: Color,
+    pub stats_error: Color,
+    pub detail_title: Color,
+}
+
+impl Default for Theme {
+    fn default() -> Self {
+        Self {
+            border: tailwind::GRAY.c600,
+            app_title: tailwind::FUCHSIA.c300,
+            stats_warning: tailwind::AMBER.c500,
+            stats_error: tailwind::RED.c500,
+            detail_title: tailwind::GRAY.c400,
+        }
+    }
+}
