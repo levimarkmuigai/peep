71cc11399d0892f45dc77ce868eb407e9db51970
levimarkmuigai <levimarkmuigai@gmail.com>
2026-05-22 08:39:45 +0300

    feat: handler unit tests

--- src/events/handler.rs
+++ src/events/handler.rs
@@ -1,6 +1,6 @@
 use crossterm::event::{Event, KeyCode, KeyEventKind};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum AppAction {
     Quit,
     SelectUp,
@@ -17,5 +17,72 @@ pub fn map_event(event: Event) -> AppAction {
             _ => AppAction::None,
         },
         _ => AppAction::None,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crossterm::event::{KeyEvent, KeyEventState, KeyModifiers};
+
+    use super::*;
+
+    #[test]
+    fn test_quit_action() {
+        let q_press = Event::Key(KeyEvent {
+            code: KeyCode::Char('q'),
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Press,
+            state: KeyEventState::empty(),
+        });
+
+        let esc_press = Event::Key(KeyEvent {
+            code: KeyCode::Esc,
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Press,
+            state: KeyEventState::empty(),
+        });
+
+        assert_eq!(map_event(q_press), AppAction::Quit);
+        assert_eq!(map_event(esc_press), AppAction::Quit);
+    }
+
+    #[test]
+    fn test_select_up() {
+        let up_press = Event::Key(KeyEvent {
+            code: KeyCode::Up,
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Press,
+            state: KeyEventState::empty(),
+        });
+
+        let k_press = Event::Key(KeyEvent {
+            code: KeyCode::Char('k'),
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Press,
+            state: KeyEventState::empty(),
+        });
+
+        assert_eq!(map_event(up_press), AppAction::SelectUp);
+        assert_eq!(map_event(k_press), AppAction::SelectUp);
+    }
+
+    #[test]
+    fn test_select_down() {
+        let down_press = Event::Key(KeyEvent {
+            code: KeyCode::Down,
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Press,
+            state: KeyEventState::empty(),
+        });
+
+        let j_press = Event::Key(KeyEvent {
+            code: KeyCode::Char('j'),
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Press,
+            state: KeyEventState::empty(),
+        });
+
+        assert_eq!(map_event(down_press), AppAction::SelectDown);
+        assert_eq!(map_event(j_press), AppAction::SelectDown);
     }
 }
