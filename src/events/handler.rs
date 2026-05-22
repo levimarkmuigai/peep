9aaaa91a808abae1399383a1c41d34091d39f5dc
levimarkmuigai <levimarkmuigai@gmail.com>
2026-05-22 08:16:43 +0300

    feat: AppActions

--- src/events/handler.rs
+++ src/events/handler.rs
@@ -1,1 +1,21 @@
+use crossterm::event::{Event, KeyCode, KeyEventKind};
 
+#[derive(Debug)]
+pub enum AppAction {
+    Quit,
+    SelectUp,
+    SelectDown,
+    None,
+}
+
+pub fn map_event(event: Event) -> AppAction {
+    match event {
+        Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
+            KeyCode::Char('q') | KeyCode::Esc => AppAction::Quit,
+            KeyCode::Up | KeyCode::Char('k') => AppAction::SelectUp,
+            KeyCode::Down | KeyCode::Char('j') => AppAction::SelectDown,
+            _ => AppAction::None,
+        },
+        _ => AppAction::None,
+    }
+}
