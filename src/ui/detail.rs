0afbfef4ae10d71d62ec6cfbbdeb681aebe4a5f2
levimarkmuigai <levimarkmuigai@gmail.com>
2026-05-27 14:50:26 +0300

    feat: details

--- src/ui/detail.rs
+++ src/ui/detail.rs
@@ -1,1 +1,42 @@
+use ratatui::{
+    Frame,
+    layout::Rect,
+    style::{Modifier, Style},
+    widgets::{Block, Borders, Paragraph, Wrap},
+};
 
+use crate::{app::AppState, ui::theme::Theme};
+
+pub fn render_details(frame: &mut Frame, area: Rect, state: &AppState, theme: &Theme) {
+    let selected_diag = state.table.selected().and_then(|i| state.items.get(i));
+
+    match selected_diag {
+        Some(d) => {
+            let file = d.file.as_deref().unwrap_or("");
+            let line = d.line.as_deref().unwrap_or("");
+
+            let details = Paragraph::new(d.message.as_str())
+                .block(
+                    Block::default()
+                        .borders(Borders::TOP)
+                        .border_style(theme.border)
+                        .title(format!(" {} : {} ", file, line))
+                        .title_style(theme.detail_title),
+                )
+                .wrap(Wrap { trim: false });
+
+            frame.render_widget(details, area);
+        }
+        None => {
+            let details = Paragraph::new("No diagnostic selected")
+                .block(
+                    Block::default()
+                        .borders(Borders::TOP)
+                        .border_style(theme.border),
+                )
+                .style(Style::new().add_modifier(Modifier::DIM));
+
+            frame.render_widget(details, area);
+        }
+    }
+}
